03d8cf159477f0e2dc7e3c3a9a850ee5d68c9055
dkfptmzld <dkfptmzld@gmail.com>
2026-06-02 21:20:52 +0900

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSy87Ek_fy1kQX18bMgU-c-wl15cQ8qsPxDw",
+  apiKey: "AIzaSyB7Ek_fy1kQX10bMgU-c-wl15cQ8qsPxDw",
   authDomain: "iruri-settlement.firebaseapp.com",
   projectId: "iruri-settlement",
   storageBucket: "iruri-settlement.firebasestorage.app",
